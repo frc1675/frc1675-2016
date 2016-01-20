ca837d4d734c57b5fb1c05c231fbd140f72f1230
Alaric <Alaric@Alarics>
2016-01-19 19:31:04 -0600

    I made a "skeleton" for the RobotMap, can be edited to fit your needs.

--- src/org/usfirst/frc/team1675/robot/RobotMap.java
+++ src/org/usfirst/frc/team1675/robot/RobotMap.java
@@ -1,18 +1,55 @@
 package org.usfirst.frc.team1675.robot;
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
+
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+
+	public static class PMWChannels {
+		public static final int EMPTY_PORT_ZERO = 0;
+		public static final int EMPTY_PORT_ONE = 1;
+		public static final int EMPTY_PORT_TWO = 2;
+		public static final int EMPTY_PORT_THREE = 3;
+		public static final int EMPTY_PORT_FOUR = 4;
+		public static final int EMPTY_PORT_FIVE = 5;
+		public static final int EMPTY_PORT_SIX = 6;
+		public static final int EMPTY_PORT_SEVEN = 7;
+		public static final int EMPTY_PORT_EIGHT = 8;
+		public static final int EMPTY_PORT_NINE = 9;
+
+	}
+
+	public static class PDChannels {
+		public static final int EMPTY_PORT_ZERO = 0;
+		public static final int EMPTY_PORT_ONE = 1;
+		public static final int EMPTY_PORT_TWO = 2;
+		public static final int EMPTY_PORT_THREE = 3;
+	}
+
+	public static class DIOChannels {
+		public static final int EMPTY_PORT_ZERO = 0;
+		public static final int EMPTY_PORT_ONE = 1;
+		public static final int EMPTY_PORT_TWO = 2;
+		public static final int EMPTY_PORT_THREE = 3;
+		public static final int EMPTY_PORT_FOUR = 4;
+		public static final int EMPTY_PORT_FIVE = 5;
+		public static final int EMPTY_PORT_SIX = 6;
+		public static final int EMPTY_PORT_SEVEN = 7;
+		public static final int EMPTY_PORT_EIGHT = 8;
+		public static final int EMPTY_PORT_NINE = 9;
+
+	}
+
+	public static class AnalogInChannels {
+		public static final int EMPTY_PORT_ZERO = 0;
+		public static final int EMPTY_PORT_ONE = 1;
+		public static final int EMPTY_PORT_TWO = 2;
+		public static final int EMPTY_PORT_THREE = 3;
+
+	}
+
+	public static class RelayChannels {
+		public static final int EMPTY_PORT_ZERO = 0;
+		public static final int EMPTY_PORT_ONE = 1;
+		public static final int EMPTY_PORT_TWO = 2;
+		public static final int EMPTY_PORT_THREE = 3;
+	}
+
 }
