ca9bf1858d3915870cf889f54c25bf81bffed8d1
FRC1675-ControlSys-1 <FRC1675-ControlSys-1@FRC1675ContSys1>
2016-01-23 14:37:53 -0600

    Converted claw arm to PID subsystem. Created a SetArmPosition command.

--- src/org/usfirst/frc/team1675/robot/OI.java
+++ src/org/usfirst/frc/team1675/robot/OI.java
@@ -11,59 +11,55 @@ import org.usfirst.frc.team1675.robot.commands.ExampleCommand;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	
+
 	public static final int OPERATOR_LEFT_Y_AXIS = 1;
 	public Joystick operatorstick;
 
-
-	
-	
-	public OI(){
+	public OI() {
 		operatorstick = new Joystick(0);
 	}
-	
-	public double getRawLeftYAxis(){
-		
+
+	public double getRawLeftYAxis() {
+
 		double leftYAxisValue = operatorstick.getRawAxis(OPERATOR_LEFT_Y_AXIS);
 		leftYAxisValue = checkForDeadzone(leftYAxisValue);
 		return leftYAxisValue;
-		
+
 	}
-    //// CREATING BUTTONS	
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
+
+	// // CREATING BUTTONS
+	// One type of button is a joystick button which is any button on a
+	// joystick.
+	// You create one by telling it which joystick it's on and which button
+	// number it is.
+	// Joystick stick = new Joystick(port);
+	// Button button = new JoystickButton(stick, buttonNumber);
 
 	private double checkForDeadzone(double leftYAxisValue) {
-		if(leftYAxisValue < 0.1675){
+		if (leftYAxisValue < 0.1675) {
 			return 0;
-		}
-		else{
+		} else {
 			return leftYAxisValue;
 		}
 	}
-    
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
-}
 
-;
+	// There are a few additional built in buttons you can use. Additionally,
+	// by subclassing Button you can create custom triggers and bind those to
+	// commands the same as any other Button.
+
+	// // TRIGGERING COMMANDS WITH BUTTONS
+	// Once you have a button, it's trivial to bind it to a button in one of
+	// three ways:
+
+	// Start the command when the button is pressed and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenPressed(new ExampleCommand());
+
+	// Run the command while the button is being held down and interrupt it once
+	// the button is released.
+	// button.whileHeld(new ExampleCommand());
+
+	// Start the command when the button is released and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenReleased(new ExampleCommand());
+};
--- src/org/usfirst/frc/team1675/robot/subsystems/ClawArm.java
+++ src/org/usfirst/frc/team1675/robot/subsystems/ClawArm.java
@@ -2,37 +2,47 @@ package org.usfirst.frc.team1675.robot.subsystems;
 
 import org.usfirst.frc.team1675.robot.RobotMap;
 import org.usfirst.frc.team1675.robot.commands.MoveWithController;
+import org.usfirst.frc.team1675.robot.commands.SetArmPosition;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.TalonSRX;
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.interfaces.Potentiometer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
-public class ClawArm extends Subsystem {
+public class ClawArm extends PIDSubsystem {
 
 	private SpeedController armMotor;
 	// Visibility (private) Type (SpeedController) Name (armMotor)
-	private Potentiometer armSensor;
+	private Encoder armSensor;
 
 	public static final int minimum = 90;
 	public static final int maximum = 200;
-
+	
+	public static final double p = 0.01;
+	public static final double i = 0;
+	public static final double d = 0;
+	
 	public ClawArm() {
-		armMotor = new TalonSRX(RobotMap.PMWChannels.EMPTY_PORT_FIVE);
-		armSensor = new AnalogPotentiometer(
-				RobotMap.DIOChannels.EMPTY_PORT_SEVEN);
+		super(p, i, d);
+
+		armMotor = new TalonSRX(RobotMap.PMWChannels.EMPTY_PORT_SIX);
+		armSensor = new Encoder(RobotMap.DIOChannels.EMPTY_PORT_ZERO,
+				RobotMap.DIOChannels.EMPTY_PORT_ONE);
 		// tells which port the talon is connecting to
+
 	}
 
 	public void moveArm(double speed) {
-		double angle = armSensor.get();
-		
 
-		if (angle >= minimum  || angle <= maximum) {
+		double angle = armSensor.get();
+
+		if (angle >= minimum || angle <= maximum) {
 			armMotor.set(speed);
 			// if angle is less or equal to 90 OR if angle is greater or equal
 			// to 200, armMotor moves x speed
@@ -45,8 +55,32 @@ public class ClawArm extends Subsystem {
 
 	}
 
+	public void setPosition(double position) {
+		this.setSetpoint(position);
+		this.enable();
+
+	}
+
+	protected double returnPIDInput() {
+
+		//SmartDashboard.putNumber("EncoderPosition", armSensor.get());
+		return armSensor.get();
+	}
+
+	protected void usePIDOutput(double output) {
+		//SmartDashboard.putNumber("MotorPower", output);
+		armMotor.set(output);
+	}
+
 	public void initDefaultCommand() {
 
 		setDefaultCommand(new MoveWithController());
+
+	}
+
+	public void stopAndDisable() {
+
+		this.disable();
+		armMotor.set(0);
 	}
 }
