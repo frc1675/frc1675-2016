d5445d75d7fa4f7f389eda8b620746476355d211
jacobboelkow <jakeboelkow1@gmail.com>
2016-01-25 18:50:25 -0600

    ClawIdle not complete, incomplete class structure

--- src/org/usfirst/frc/team1675/robot/subsystems/ClawSubSystem.java
+++ src/org/usfirst/frc/team1675/robot/subsystems/ClawSubSystem.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team1675.robot.subsystems;
 
+import org.usfirst.frc.team1675.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.TalonSRX;
@@ -9,46 +11,40 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class ClawSubSystem extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-	
-	private SpeedController rollerIn;
-	private SpeedController rollerOut;
-	
-	private DigitalInput limitSwitch;
-	
-	private final int ROLLER_IN_PORT = 0; //Set to 0 and 1 as filler
-	private final int ROLLER_OUT_PORT = 1; //Find actual numbers needed
-	
-	public ClawSubSystem(){
-		rollerIn = new TalonSRX(ROLLER_IN_PORT);
-		rollerOut = new TalonSRX(ROLLER_OUT_PORT);
-		limitSwitch = new DigitalInput(1); //1 is a filler. Find actual number later
-	}
-	
-	public void setRollerInPower(double value){
-		rollerIn.set(value);
-	}
-	
-	public void setRollerOutPower(double value){
-		rollerOut.set(value);
-	}
-	
-	public boolean hasBall(){
-		if(limitSwitch.get() == true){ //Change if no limit switch
-			return true;
-		} 
-		return false;
-//		else if(buttonA.isPressed){ //buttonA is filler until we have controller map
-//			return true;
-//		} else{
-//			return false;
-//		}
+
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	private SpeedController rollerTop;
+	private SpeedController rollerBottom;
+
+	public ClawSubSystem() {
+		rollerTop = new TalonSRX(RobotMap.PMWChannels.EMPTY_PORT_ZERO);
+		rollerBottom = new TalonSRX(RobotMap.PMWChannels.EMPTY_PORT_ONE);
 	}
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+	public void setRollerInPower(double value) {
+		rollerTop.set(value);
+		rollerBottom.set(-value);
+	}
+
+	public void setRollerOutPower(double value) {
+		rollerTop.set(value);
+		rollerBottom.set(-value);
+
+	}
+
+	public boolean hasBall() {
+		
+//		if (buttonA.isPressed) { // buttonA is filler until we have controller
+//									// map
+//			return true;
+//		} else
+			return false;
+	}
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
 }
