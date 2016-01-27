2f9f488d370fdceafce52a00ac6f4124d7a1ef9b
Brendan O'Brien <bwobkj@gmail.com>
2016-01-26 20:00:02 -0600

    Testing command groups

--- src/org/usfirst/frc/team1675/robot/commands/ClawIdle.java
+++ src/org/usfirst/frc/team1675/robot/commands/ClawIdle.java
@@ -21,7 +21,7 @@ public class ClawIdle extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.clawSub.setRollerInPower(0); //Set actual value later. 1 is a filler
+    	Robot.clawSub.setRollerInPower(0); 
     }
 
     // Make this return true when this Command no longer needs to run execute()
