e3bbcdc0caf61db57849e49fde651c097eb9cae0
Fatima Odum <farocks08@gmail.com>
2016-02-06 11:43:42 -0600

    created a ClawIntakeForTime and a ClawOutputForTime commands.

--- src/org/usfirst/frc/team1675/robot/commands/ClawIntake.java
+++ src/org/usfirst/frc/team1675/robot/commands/ClawIntake.java
@@ -41,5 +41,6 @@ public class ClawIntake extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	this.end();
     }
 }
--- src/org/usfirst/frc/team1675/robot/commands/Wait.java
+++ src/org/usfirst/frc/team1675/robot/commands/Wait.java
@@ -42,10 +42,12 @@ public class Wait extends Command {
     // Called once after isFinished returns true
     protected void end() {
     	waitTimer.stop();
+    	waitTimer.reset();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	this.end();
     }
 }
