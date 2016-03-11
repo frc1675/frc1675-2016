7d293da6bc854160c4a0672f17e08867eca1d291
Nosirah <unshavedmonkey8@gmail.com>
2016-03-10 23:21:04 -0600

    Added a command group for Autonomously going over the Cheval de Frise

---
+++ src/org/usfirst/frc/team1675/robot/commands/auto/FrenchRampsAuto.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team1675.robot.commands.auto;
+
+import org.usfirst.frc.team1675.robot.commands.Wait;
+import org.usfirst.frc.team1675.robot.commands.clawarm.ClawArmMoveForXSeconds;
+import org.usfirst.frc.team1675.robot.commands.drivebase.DriveForDistance;
+import org.usfirst.frc.team1675.robot.commands.drivebase.DriveStraightForTime;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class FrenchRampsAuto extends CommandGroup {
+    
+    public  FrenchRampsAuto() {
+    	addSequential(new DriveForDistance(24));
+    	addSequential(new ClawArmMoveForXSeconds(0.4, -0.3));//Lower claw arm here will go
+    	addSequential(new Wait(1.5));
+    	addSequential(new DriveStraightForTime(1, 1));
+
+       
+    }
+}
