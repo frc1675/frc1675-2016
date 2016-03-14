e695bab972789d9fe352c0f1272a46785d207239
Josh Pordon <josh.pordon@gmail.com>
2016-03-13 22:33:50 -0500

    can i use emotes in these :robot_face:

--- src/org/usfirst/frc/team1675/robot/commands/auto/RockWallAuto.java
+++ src/org/usfirst/frc/team1675/robot/commands/auto/RockWallAuto.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class RockWallAuto extends CommandGroup {
     
     public  RockWallAuto() {
-    	//Not yet tested!
+    	//Not yet tested! for real
         addSequential(new DriveStraightForTime(1.0, 2.0));
         addParallel(new DriveStraightForTime(1.0, 2.0));
         addSequential(new ClawArmMoveForXSeconds(0.4, -0.3));
