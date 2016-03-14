c6ac5710cf27a8bef54308931e5a76ddd26bb2ce
Josh Pordon <josh.pordon@gmail.com>
2016-03-13 22:31:57 -0500

    added comment and testing github-slack integration

--- src/org/usfirst/frc/team1675/robot/commands/auto/RockWallAuto.java
+++ src/org/usfirst/frc/team1675/robot/commands/auto/RockWallAuto.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class RockWallAuto extends CommandGroup {
     
     public  RockWallAuto() {
+    	//Not yet tested!
         addSequential(new DriveStraightForTime(1.0, 2.0));
         addParallel(new DriveStraightForTime(1.0, 2.0));
         addSequential(new ClawArmMoveForXSeconds(0.4, -0.3));
