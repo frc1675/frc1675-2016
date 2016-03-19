79f16080125d38597853c1e1642e50a2de821a1e
Josh Pordon <josh.pordon@gmail.com>
2016-03-19 12:26:02 -0500

    working practice bot code

--- src/org/usfirst/frc/team1675/robot/subsystems/ClawArm.java
+++ src/org/usfirst/frc/team1675/robot/subsystems/ClawArm.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class ClawArm extends Subsystem {
@@ -78,13 +79,15 @@ public class ClawArm extends Subsystem {
 	}
 
 	private void moveWithinLimitSwitches(SpeedController sc, double power) {
-		if (getLimitValueUp() == true) {
+		//stuff wired wrong on practice robot fix for competition
+		power = -power;
+		if (getLimitValueDown() == true) {
 			if (power > 0) {
 				sc.set(power);
 			} else {
 				sc.set(0);
 			}
-		} else if (getLimitValueDown() == true) {
+		} else if (getLimitValueUp() == true) {
 			if (power < 0) {
 				sc.set(power);
 			} else {
